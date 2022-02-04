bfa957083a71e4df2ff92cdad6048c143591cefc
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-02-04 17:06:07 +0100

    1.4 accessory 2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,6 +11,14 @@ public class DefaultCountingOutRhymer {
 
 	private int total = EMPTY_TOTAL;
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
