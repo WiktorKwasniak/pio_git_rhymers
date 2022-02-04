430632d2b27e82ddb8c385784622a81eb46bcee2
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-02-04 17:07:43 +0100

    1.4 accessory 3

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -4,7 +4,7 @@ public class IntLinkedList {
 
 	private static final int ERROR_CODE = -1;
 	Node last;
-	int i;
+	private int i;
 
 	public void push(int i) {
 		if (last == null)
@@ -27,15 +27,23 @@ public class IntLinkedList {
 	public int top() {
 		if (isEmpty())
 			return ERROR_CODE;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return ERROR_CODE;
-		int ret = last.value;
+		int ret = last.getValue();
 		last = last.prev;
 		return ret;
+	}
+
+	int getI() {
+		return i;
+	}
+
+	void setI(int i) {
+		this.i = i;
 	}
 
 }
